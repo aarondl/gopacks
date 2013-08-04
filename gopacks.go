9900f571ca4d810437488e57d99231c149ae5eaf
Aaron L <aaron@bettercoder.net>
2013-08-04 16:58:17 +0900

    Add placeholder fcgi server.

--- gopacks.go
+++ gopacks.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/http/fcgi"
+	"os"
+	"os/signal"
+	"strconv"
+)
+
+var html = `<!DOCTYPE html>
+<html>
+	<head>
+		<title>Gopacks.org</title>
+	</head>
+	<body>
+		<h1>Gopacks.org coming soon...</h1>
+	</body>
+</html>`
+
+var portFlag = flag.Int("port", 80, "The port on which to listen.")
+
+type server struct {
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%v requested: %v", r.Host, r.RequestURI)
+	headers := w.Header()
+	headers.Add("Content-Type", "text/html")
+	io.WriteString(w, html)
+}
 
 func main() {
-	fmt.Println("Hello package world.")
+	flag.Parse()
+
+	exit := make(chan os.Signal)
+	signal.Notify(exit, os.Interrupt, os.Kill)
+
+	port := ":" + strconv.Itoa(*portFlag)
+
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalln("Error listening:", err)
+	}
+	defer listener.Close()
+	log.Println("Listening on:", *portFlag)
+
+	log.Println("Starting service...", *portFlag)
+	go func() {
+		err = fcgi.Serve(listener, &server{})
+		if err != nil {
+			log.Println("FCGI Serve error:", err)
+			exit <- nil
+		}
+	}()
+
+	sig := <-exit
+	if sig != nil {
+		log.Println("Received signal:", sig)
+	}
+	log.Println("Exiting.")
 }
